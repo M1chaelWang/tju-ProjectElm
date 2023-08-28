348d6bc521e520b7fb11dd7e7c1257798910bfa8
M1chaelWang <2239190845@qq.com>
2023-08-28 17:55:10 +0800

    lab3_server done

--- elm/src/com/neusoft/elm/service/OrdersService.java
+++ elm/src/com/neusoft/elm/service/OrdersService.java
@@ -1,13 +1,5 @@
 package com.neusoft.elm.service;
 
-<<<<<<< HEAD
-import com.neusoft.elm.po.Orders;
-
-public interface OrdersService {
-	
-	public int createOrders(String userId, Integer businessId, Integer daId, Double orderTotal);
-	public Orders getOrdersById(Integer orderId);
-=======
 import java.util.List;
 
 import com.neusoft.elm.po.Orders;
@@ -20,5 +12,4 @@ public interface OrdersService {
 
 	// 查询历史订单
 	public List<Orders> listOrdersByUserId(String userId);
->>>>>>> 1af82b4f7ad252677d10555067ef2457aeadf70e
 }
