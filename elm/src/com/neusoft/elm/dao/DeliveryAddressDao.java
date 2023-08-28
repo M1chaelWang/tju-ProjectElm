1af82b4f7ad252677d10555067ef2457aeadf70e
guoyan255 <1161647924@qq.com>
2023-08-28 17:22:42 +0800

    gy

--- elm/src/com/neusoft/elm/dao/DeliveryAddressDao.java
+++ elm/src/com/neusoft/elm/dao/DeliveryAddressDao.java
@@ -6,5 +6,14 @@ import com.neusoft.elm.po.DeliveryAddress;
 
 public interface DeliveryAddressDao {
 	
+	//根据UserId查询送货地址
 	public List<DeliveryAddress> listDeliveryAddressByUserId(String userId) throws Exception;
+	//添加送货地址
+	public int saveDeliveryAddress(DeliveryAddress deliveryAddress) throws Exception;
+	//根据ID查询送货地址
+	public DeliveryAddress getDeliveryAddressById(Integer daId) throws Exception;
+	//更新送货地址
+	public int updateDeliveryAddress(DeliveryAddress deliveryAddress) throws Exception;
+	//移除送货地址
+	public int removeDeliveryAddress(Integer daId) throws Exception;
 }
--- elm/src/com/neusoft/elm/dao/UserDao.java
+++ elm/src/com/neusoft/elm/dao/UserDao.java
@@ -3,6 +3,12 @@ package com.neusoft.elm.dao;
 import com.neusoft.elm.po.User;
 
 public interface UserDao {
-	
+
 	public User getUserByIdByPass(String userId, String password) throws Exception;
+
+	// 查询手机号码是否已注册，只查询有没有
+	public int getUserById(String userId) throws Exception;
+
+	// 添加用户信息
+	public int saveUser(User user) throws Exception;
 }
