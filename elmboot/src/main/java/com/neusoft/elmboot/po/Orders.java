ec63748c8114b5fff716733a94f727853dec80af
guoyan255 <1161647924@qq.com>
2023-08-31 17:51:14 +0800

    gy

---
+++ elmboot/src/main/java/com/neusoft/elmboot/po/Orders.java
@@ -0,0 +1,57 @@
+package com.neusoft.elmboot.po;
+
+public class Orders {
+	
+	private Integer orderId;
+	private String userId;
+	private Integer businessId;
+	private String orderDate;
+	private Double orderTotal;
+	private Integer daId;
+	private Integer orderState;
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Integer getBusinessId() {
+		return businessId;
+	}
+	public void setBusinessId(Integer businessId) {
+		this.businessId = businessId;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public Double getOrderTotal() {
+		return orderTotal;
+	}
+	public void setOrderTotal(Double orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+	public Integer getDaId() {
+		return daId;
+	}
+	public void setDaId(Integer daId) {
+		this.daId = daId;
+	}
+	public Integer getOrderState() {
+		return orderState;
+	}
+	public void setOrderState(Integer orderState) {
+		this.orderState = orderState;
+	}
+	
+	
+}
---
+++ elmboot/src/main/java/com/neusoft/elmboot/service/FoodService.java
@@ -0,0 +1,9 @@
+package com.neusoft.elmboot.service;
+
+import java.util.List;
+
+import com.neusoft.elmboot.po.Food;
+
+public interface FoodService {
+    public List<Food> listFoodByBusinessId(Integer businessId);
+}
